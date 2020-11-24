a23c1965f920f7ecbd89139092c1ff3f10f97da2
mSolobodowski <mateusz.solobodowski94@gmail.com>
2020-11-24 21:05:02 +0100

    scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
+    if ($(window).width() > 850)
+    {
+        getTallestHeaderHeight('.products__itemTitle');
+        getTallestHeaderHeight('.products__itemSubtitle');
+    }
+
+    // $( window ).resize(function() {
+    //     if ($(window).width() > 850)
+    //     {
+    //         getTallestHeaderHeight('.products__itemTitle');
+    //         getTallestHeaderHeight('.products__itemSubtitle');
+    //     }
+    // });
+
+    function getTallestHeaderHeight(itemElement) {
+         let max = 0;
+        $(itemElement).each(function(){
+            if ($(this).innerHeight() > max)
+                max = $(this).innerHeight();
+        })
+        $(itemElement).css('min-height', max);
+     
+
+    }
 });
